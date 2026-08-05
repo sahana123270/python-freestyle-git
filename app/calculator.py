7850cb089a2c2d7260dc1ddf089b1956cbfe85e2
sahana123270 <sahana.s1@tataelxsi.co.in>
2026-08-05 19:04:14 +0530

    code updated

---
+++ app/calculator.py
@@ -0,0 +1,10 @@
+def add(a,b):
+    return a+b
+ 
+ 
+def subtract(a,b):
+    return a-b
+ 
+if __name__=="__main__":
+    print("add result",add(10,20))
+    print("subtraction result", subtract(20,5))
