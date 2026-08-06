0cdce721c04099ac47e0fdc1ca61aa306185b82c
sahana123270 <sahana.s1@tataelxsi.co.in>
2026-08-06 12:09:16 +0530

    updated message

--- app/calculator.py
+++ app/calculator.py
@@ -6,5 +6,5 @@ def subtract(a,b):
     return a-b
  
 if __name__=="__main__":
-    print("add result",add(10,20))
-    print("subtraction result", subtract(20,5))
+    print("add result",add(10,50))
+    print("subtraction result", subtract(20,10))
