f6864b6faebba2866267fe6bcd1d4b061e968ea0
sahana123270 <sahana.s1@tataelxsi.co.in>
2026-08-06 12:56:16 +0530

    added calculator tests

--- app/calculator.py
+++ app/calculator.py
@@ -4,6 +4,14 @@ def add(a,b):
  
 def subtract(a,b):
     return a-b
+
+def multiply(a,b):
+    return a *b
+ 
+def division(a,b):
+    if b==0:
+        raise ValueError("cannot divide by 0")
+    return a/b
  
 if __name__=="__main__":
     print("add result",add(10,50))
