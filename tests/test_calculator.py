5b65ff9dac2a6a17a522c1903d40665134b4386b
sahana123270 <sahana.s1@tataelxsi.co.in>
2026-08-08 16:42:00 +0530

    added regression and smoke

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,7 +2,7 @@ import pytest
  
 from app.calculator import add,subtract,multiply,division
  
- 
+@pytest.mark.smoke 
 def test_add():
     assert add(10,20)==30
  
@@ -11,10 +11,12 @@ def test_subtract():
  
 def test_multiply():
     assert multiply(4,4)==16
- 
+
+@pytest.mark.regression 
 def test_division():
     assert division(20,5)==4
- 
+
+@ pytest.mark.regression
 def test_divide_by_zero():
     with pytest.raises(ValueError):
         division(10,0)
